fc11a893c4e9803d4544c12be96ff15744fcfd32
helvioco-source <helvioco@gmail.com>
2026-08-18 19:24:01 -0300

    Atualiza numero do WhatsApp

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
       </nav>
 
       <a
-        href="https://wa.me/5562982937272?text=Ol%C3%A1!%20Gostaria%20de%20agendar%20uma%20demonstra%C3%A7%C3%A3o%20do%20VaqtorApp%20e%20conhecer%20como%20o%20sistema%20pode%20ajudar%20no%20gerenciamento%20da%20minha%20fazenda."
+        href="https://wa.me/5564992831256?text=Ol%C3%A1!%20Gostaria%20de%20agendar%20uma%20demonstra%C3%A7%C3%A3o%20do%20VaqtorApp%20e%20conhecer%20como%20o%20sistema%20pode%20ajudar%20no%20gerenciamento%20da%20minha%20fazenda."
         target="_blank"
         rel="noopener noreferrer"
         className="btn-demo"
